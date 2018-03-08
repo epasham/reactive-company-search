35315d7dc9b7e9a159a11f427e2f2ba391a91543
Ekambaram Pasham <epasham@users.noreply.github.com>
2018-03-08 22:09:14 +0530

    Update CompanyRepository.java

--- src/main/java/com/example/companysearch/repository/CompanyRepository.java
+++ src/main/java/com/example/companysearch/repository/CompanyRepository.java
@@ -1,11 +1,17 @@
 package com.example.companysearch.repository;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
 
 public interface CompanyRepository extends ReactiveCrudRepository<Company, String> {
 
-  Flux<Company> findByName(String name);
+  Mono<Company> findByName(String name);
+  
+  Flux<Company> findCompanyByCountry(String country);
+  
+  @Query("{'state' : ?0, 'country' : ?1}")
+	Flux<Company> findByStateAndCountry(String state, String country);
   
 }
