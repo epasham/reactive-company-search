8574d83c57cf6a9770bac9bed4ddae6d63345cf6
Ekambaram Pasham <epasham@users.noreply.github.com>
2018-03-09 15:52:04 +0530

    Update CompanyHandler.java

--- src/main/java/com/example/companysearch/handler/CompanyHandler.java
+++ src/main/java/com/example/companysearch/handler/CompanyHandler.java
@@ -6,16 +6,20 @@ import com.example.companysearch.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
 public class CompanyHandler {
 
-  @Autowired
+	@Autowired
 	private CompanyRepository repository;
 
-  public Mono<ServerResponse> getAll(ServerRequest request) {
+  	public Mono<ServerResponse> getAll(ServerRequest request) {
 		Flux<Company> company = repository.findAll();
 		return ServerResponse.ok().body(BodyInserters.fromPublisher(company, Company.class));
 	}
