53087cacfd1c23b0e0a626da833f6b6d1ce38d53
Ekambaram Pasham <epasham@users.noreply.github.com>
2018-03-09 19:00:42 +0530

    Update SwaggerConfig.java

--- src/main/java/com/example/companysearch/config/SwaggerConfig.java
+++ src/main/java/com/example/companysearch/config/SwaggerConfig.java
@@ -1,2 +1,9 @@
 package com.example.companysearch.config;
 
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+}
