bf7dab90cfa297d8b54a2a81efdfe9713172bb83
Ekambaram Pasham <epasham@users.noreply.github.com>
2018-03-09 16:06:35 +0530

    Create RouteConfig.java

---
+++ src/main/java/com/example/companysearch/config/RouteConfig.java
@@ -0,0 +1,24 @@
+package com.example.companysearch.config;
+
+import com.example.companysearch.handler.CompanyHandler;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class RouteConfig {
+
+	@Bean
+	RouterFunction<ServerResponse> routes(CompanyHandler handler) {
+
+		return route(GET("/company"),
+							request -> handler.getAll(request))
+
+	}
+}
