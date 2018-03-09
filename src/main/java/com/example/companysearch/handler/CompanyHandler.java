f3b3faf5ed41ded651a7d99bf20df8cd7079bb29
Ekambaram Pasham <epasham@users.noreply.github.com>
2018-03-09 14:05:16 +0530

    Create CompanyHandler.java

---
+++ src/main/java/com/example/companysearch/handler/CompanyHandler.java
@@ -0,0 +1,19 @@
+package com.example.companysearch.handler;
+
+import com.example.companysearch.model.Company;
+import com.example.companysearch.repository.CompanyRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyHandler {
+
+
+
+
+
+
+
+
+}
